9e078caa1b8730408bdc98bb9130e7eda436ac87
Linus Norton <linusnorton@gmail.com>
2017-05-23 21:10:11 +0100

    Added check for id properties

--- README.md
+++ README.md
@@ -17,9 +17,24 @@ npm install --save memoized-class-decorator
 import memoize from 'memoized-class-decorator';
 
 class Foo {
+
+  constructor(private num: number) { }
+
   @memoize
-  myMethod() {
-    // ...
+  myMethod(add: number) {
+    return this.num + add;
   }
 }
+
+const f1 = new Foo(5);
+
+f1.myMethod(5); //= 10 (method is invoked)
+f1.myMethod(5); //= 10 (method is not invoked)
+
+const f2 = new Foo(0);
+
+f1.myMethod(5); //= 5 (method is invoked, each object has it's own cache)
+
 ```
+
+Note that if an object is passed to the method with an `id` property, the value of that propery will be used as the cache key. This is to reduce the time needed to serialize complex objects.
--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 module.exports = function(target, key, descriptor) {
-  var type  = descriptor.get ? 'get' : 'value';
-  var fn    = descriptor[type];
-  var char0 = String.fromCharCode(0);
+  const fType  = descriptor.get ? 'get' : 'value';
+  const fn    = descriptor[fType];
+  const char0 = String.fromCharCode(0);
 
-  descriptor[type] = function(){
-    var keyAry = [];
+  descriptor[fType] = function(){
+    const keyAry = [];
 
     if (!this.hasOwnProperty("__memoized__")) {
       this.__memoized__ = {};
     }
 
     for (let i=0, l=arguments.length; i<l; i++) {
-      let arg  = arguments[i];
-      let type = typeof arg;
+      const arg  = arguments[i];
+      const type = typeof arg;
 
       keyAry.push(
-        (arg  === null)       ? char0 + 'null'      :
-        (arg  === void 0)     ? char0 + 'undefined' :
-        (type === 'function') ? char0 + arg         :
-        (type === 'object')   ? char0 + JSON.stringify(arg) :
+        (arg  === null)               ? char0 + 'null'      :
+        (arg  === void 0)             ? char0 + 'undefined' :
+        (type === 'function')         ? char0 + arg         :
+        (type === 'object' && arg.id) ? char0 + arg.id      :
+        (type === 'object')           ? char0 + JSON.stringify(arg) :
         arg
       );
     }
 
-    var cacheKey = key + keyAry.join(String.fromCharCode(0));
+    const cacheKey = key + keyAry.join(String.fromCharCode(0));
 
     if (!this.__memoized__.hasOwnProperty(cacheKey)) {
         this.__memoized__[cacheKey] = fn.apply(this, arguments);
@@ -34,3 +35,4 @@ module.exports = function(target, key, descriptor) {
 
   return descriptor;
 };
+
